c573ab0bad6bfe65b9c50908d3a7cd436bc6acaa
bakariL <bakarilewis@gmail.com>
2019-04-06 14:58:28 -0400

    login error1.12 fix

--- Startup.cs
+++ Startup.cs
@@ -45,12 +45,12 @@ namespace ckl
         public void ConfigureServices(IServiceCollection services)
         {
         
-            // services.Configure<CookiePolicyOptions>(options =>
-            //  {
-            //     // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-            //     options.CheckConsentNeeded = context => true;
-            //      options.MinimumSameSitePolicy = SameSiteMode.None;
-            //  });
+            services.Configure<CookiePolicyOptions>(options =>
+             {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                 options.MinimumSameSitePolicy = SameSiteMode.None;
+             });
             var connectionString = Configuration.GetConnectionString("DefaultConnection");
 
             services.AddEntityFrameworkNpgsql()
@@ -78,12 +78,12 @@ namespace ckl
                 options.Lockout.AllowedForNewUsers = true;
             });
 
-             services.ConfigureApplicationCookie(options =>
-            {
-        options.LoginPath = $"/Identity/Account/Login";
-        options.LogoutPath = $"/Identity/Account/Logout";
-        options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
-             });
+        //      services.ConfigureApplicationCookie(options =>
+        //     {
+        // options.LoginPath = $"/Identity/Account/Login";
+        // options.LogoutPath = $"/Identity/Account/Logout";
+        // options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+        //      });
 
 
             services.AddMvc(config => {
